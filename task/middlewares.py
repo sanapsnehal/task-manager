b5e781a8770cddf6902766ee47d05b478932e0b5
sanapsnehal <snehalsanap786@gmail.com>
2022-03-16 14:56:09 +0530

    middle

--- task/middlewares.py
+++ task/middlewares.py
@@ -1,19 +1,29 @@
 from datetime import datetime
-from django.shortcuts import render
+from urllib import response
+from django import views
+from django.shortcuts import render,HttpResponse,HttpResponseRedirect
 
 from .models import RequestLog
+from datetime import datetime
 
+obj_now = datetime.now()
+time = obj_now.strftime("%H:%M")
+time = str(time)
+class UnderConstructionMiddleware:
+	def __init__(self,get_response):
+		self.get_response=get_response
 
+	def __call__(self,request):
+		if (time >= "10:46" and time < "17:00"):
+			return render(request, 'underconstruction.html')
+		else:
+			return render(request,'home.html')
 
-
-class UnderConstructionsMiddleware:
-    def __init__(self,get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-         return render(request, 'task/underconstruction.html')
-
-
+		# print("call From Middleware Before views")
+		# response= render(request, 'underconstruction.html')
+		# # response = HttpResponse("this site is underconstruction")
+		# print("call from middleware after view")
+		# return response
 
 
 
@@ -31,8 +41,10 @@ class UnderConstructionsMiddleware:
 def RequestLogMiddleware(get_response):
 	def my_function(request):
 		response = get_response(request)
-		user = 'snehal'
-		rl = RequestLog(name=user, timestamp = datetime.now)
+		# user = 'snehal'
+		if not request.user:
+			return HttpResponseRedirect('login.html')
+		rl = RequestLog(name=request.user, timestamp = datetime.now)
 		rl.save()
 		return response
 	return my_function
